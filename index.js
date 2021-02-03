64968291507ed711473eca5d2b8a43cb71c8b803
jomapormentilla <jomapormentilla@gmail.com>
2021-02-02 23:51:55 -0500

    commit tree ed09a42f04eb1380e3de6caf7da8e95db6799b10

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 20
+let num4 = 16
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = num3 % num4
+
+const max = Math.max(1,5,13,18,20)
